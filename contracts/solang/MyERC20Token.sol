1578e65b7dfcbee4b949fb1e852e704814ac77d3
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2022-04-22 10:19:57 +0200

    Update MyERC20Token.sol

--- contracts/solang/MyERC20Token.sol
+++ contracts/solang/MyERC20Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import "./ERC20.sol";
 
-contract MyToken is ERC20 {
+contract MyERC20Token is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
 
         _mint(msg.sender, 100000 * 10**uint(decimals()));
